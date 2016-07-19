58ac0b1aaad40fa2a3cd69e5f158398813aaf52c
beston-mawaire <beston@rapidtrade.biz>
2016-07-19 09:56:00 +0200

    changes

--- js/sginspect/controllers/SgInspectUserCtrl.js
+++ js/sginspect/controllers/SgInspectUserCtrl.js
@@ -7,33 +7,18 @@ coreApp.controller("SgInspectUserCtrl",function($scope,$route,$routeParams,$http
     var user = GlobalSvc.getUser();
 
     function newUserObject(){
-        var newUser = {};
-        newUser.SupplierID = user.SupplierID;
-        newUser.UserID = "";
-        newUser.Name = "";
-        newUser.PasswordHash = "";
-        newUser.PasswordSalt = null;
-        newUser.AddressID = "";
-        newUser.IsLockedOut = "";
-        newUser.LastLoginDate = "";
-        newUser.FailedPasswordAttemptCount = "";
-        newUser.Email = "";
-        newUser.Country = null;
-        newUser.Deleted = 0;
-        newUser.RepID = null;
-        newUser.Manager = null;
-        newUser.IsAdmin = false;
-        newUser.IsRep =false;
-        newUser.IsManager = false;
-        newUser.DailySummary = false;
-        newUser.WeeklySummary = false;
-        newUser.MonthlyKPI = false;
-        newUser.AllCustomers = false;
-        newUser.NumCustomers = false;
-        newUser.Role = null;
-        newUser.Tel = "";
-
-        return newUser;
+        var user = {};
+        user.UserID = "";
+        user.PasswordHash = "";
+        user.Name = "";
+        user.IsLockedOut = "";
+        user.LastLoginDate = "";
+        user.Email = "";
+        user.Active = 1;
+        user.IsAdmin = false;
+        user.Tel = "";
+        user.Role = null;
+        return user;
     }
 
     $scope.deleteUser = function(){
@@ -58,6 +43,7 @@ coreApp.controller("SgInspectUserCtrl",function($scope,$route,$routeParams,$http
 
     $scope.saveUser = function(){
         savebtnClicked = true;
+        $scope.$emit('LOAD');
         if($routeParams.id === 'new'){
             //Checking if the user already exists //this Code Could Be Improved
             $http.get(Settings.url + 'GetStoredProc?StoredProc=usp_user_readsingle&params=("'+ $scope.userEdit.UserID +'")').success(function(data){
@@ -66,6 +52,7 @@ coreApp.controller("SgInspectUserCtrl",function($scope,$route,$routeParams,$http
                     delete $scope.errorMsg;
                     delete $scope.successMsg;
                     $alert({ content: "User already exists!", duration: 4, placement: 'top-right', type: 'danger', show: true});
+                    $scope.$emit('UNLOAD');
                     return;
                 }else{
                     save();
@@ -77,18 +64,14 @@ coreApp.controller("SgInspectUserCtrl",function($scope,$route,$routeParams,$http
     };
 
     function save(){
-        $scope.$emit('LOAD');
         sessionStorage.removeItem( "UsersCache");
-        var url = Settings.url + 'StoredProcModify?StoredProc=usp_user_modify';
-		// for backward compatibility
-		$scope.userEdit.PasswordSalt = $scope.userEdit.Role;
-
+        var url = Settings.url + 'StoredProcModify?StoredProc=User_modify';
         GlobalSvc.postData(url,$scope.userEdit,function(){
             $scope.$emit('UNLOAD');
             $scope.successMsg = 'User saved Ok';
             sessionStorage.removeItem("UsersCache");
-            $scope.$apply();
             $location.path('/SgUsers');
+            $scope.$apply();
         },function(){
             $scope.$emit('UNLOAD');
             $scope.errorMsg = 'Error saving user';
@@ -112,15 +95,12 @@ coreApp.controller("SgInspectUserCtrl",function($scope,$route,$routeParams,$http
         console.log($scope.users);
     }
 
-    function fetchUser(userid){
-        userid = userid || $routeParams.id;
+    function fetchUser(){
         if($routeParams.id === 'new' && !savebtnClicked){
-            //newUserObject();
-            //$scope.userEdit = JSON.parse(sessionStorage.getItem('currentuseredit'));
             $scope.userEdit = newUserObject();
             $scope.$emit('UNLOAD');
         }else{
-            var url = Settings.url + 'GetStoredProc?StoredProc=usp_user_readsingle&params=("'+ userid +'")';
+            var url = Settings.url + 'GetStoredProc?StoredProc=usp_user_readsingle&params=("'+ $routeParams.id +'")';
             console.log(url);
             $http.get(url).success(function(data){
                 //get the First Object because it comes back because it is what stores the user data
