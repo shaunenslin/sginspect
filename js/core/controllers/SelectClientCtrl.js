eade275a8d4531163ea000e3b6b32de23592ca41
Tevin <tevin@rapidtrade.biz>
2016-07-26 22:56:31 +0200

    changes

--- js/core/controllers/SelectClientCtrl.js
+++ js/core/controllers/SelectClientCtrl.js
@@ -1,5 +1,7 @@
-coreApp.controller('SelectClientCtrl', function($scope, GlobalSvc, DaoSvc, Settings, $http, $alert, $routeParams, $location, CaptureImageSvc){
+coreApp.controller('SelectClientCtrl', function($scope, GlobalSvc, DaoSvc, Settings, $http, $alert, $routeParams, $location, CaptureImageSvc, JsonFormSvc){
 	$scope.isPhoneGap = Settings.isPhoneGap;
+	$scope.Uploadmessage = "Attach Image files";
+	$scope.image = "";
 
 	function newObject(){
 		$scope.Form = {
@@ -12,7 +14,7 @@ coreApp.controller('SelectClientCtrl', function($scope, GlobalSvc, DaoSvc, Setti
 			JSON 		: {
 				"UserField01" : "",
 				"UserField02" : "",
-				"UserField03" : moment().format('YYYY-MM-DD HH:mm:ss'),
+				"UserField03" : "",
 				"UserField04" : "",
 				"UserField05" : "",
 				"UserField06" : "",
@@ -28,12 +30,13 @@ coreApp.controller('SelectClientCtrl', function($scope, GlobalSvc, DaoSvc, Setti
 				"UserField16" : "",
 				"UserField17" : "",
 				"UserField18" : "",
-				"UserField19" : moment().format('YYYY-MM-DD HH:mm:ss'),
+				"UserField19" : "",
 				"UserField20" : "",
 				"UserField21" : "",
 				"UserField22" : "",
 				"UserField23" : ""
-		}		}
+			}
+		}
 	}
 
 	$scope.onNextClicked = function(){
@@ -105,15 +108,19 @@ coreApp.controller('SelectClientCtrl', function($scope, GlobalSvc, DaoSvc, Setti
 		$alert({content: "Vehicle number " + $scope.VinNumber + " scanned successfully. Please Press 'Next' to continue", duration:6, placement:'top-right', type:'success', show:true});
 	}
 
-	$scope.onPhotoClicked = function(){
+	$scope.onPhotoClicked = function(field){
 		var reader = new FileReader();
 		reader.addEventListener("load", function () {
 			$scope.image = reader.result;
-			$scope.licenceImage = reader.result;
-			$scope.capture = true;
+			if ($routeParams.screennum == 6 && $scope.image){
+				var key = $scope.Form.FormID + '_' + field + '.png';
+				CaptureImageSvc.savePhoto(key, $scope.image);
+			} else{
+				$scope.capture = true;
+			}
 			$alert({content:"Image captured successfully", duration:6, placement:'top-right', type:'success', show:true});
 			$scope.$apply();
-  		}, false);
+			}, false);
 		if ($scope.isPhoneGap){
 			var onSuccess = function(img){
 				reader.readAsDataURL(img);
@@ -126,8 +133,8 @@ coreApp.controller('SelectClientCtrl', function($scope, GlobalSvc, DaoSvc, Setti
 			var image = $('#file-select')[0];
 
 			if (image){
-        		reader.readAsDataURL(image.files[0]);
-        	}
+	    		reader.readAsDataURL(image.files[0]);
+	    	}
 		}
 	}
 
@@ -142,9 +149,25 @@ coreApp.controller('SelectClientCtrl', function($scope, GlobalSvc, DaoSvc, Setti
 		$scope.$apply();
 	}
 
+	function fetchGPs(){
+		GlobalSvc.getGPS(function(position){
+			$scope.Form.Latitude  = position ? position.coords.latitude : "";
+			$scope.Form.Longitude = position ? position.coords.longitude : "";
+			$scope.$emit('UNLOAD');
+		},function(error){
+			console.log('error! ' + error);
+			$scope.formitem.Latitude  = '';
+			$scope.formitem.Longitude = '';
+			$scope.$emit('UNLOAD');
+		});
+	}
+
+	$scope.attachmentClicked = function(){angular.element("#file-select").trigger('click');};
+
 
 	function constructor(){
 		$scope.$emit('LOAD');
+		$scope.inspectiontype = $routeParams.inspectiontype;
 		$scope.$emit('heading',{heading: 'Audit Form', icon : 'fa fa-check-square-o'});
 		$scope.$emit('left',{label: 'Back' , icon : 'fa fa-chevron-left', onclick: $scope.onBackClicked});
         $scope.$emit('right', {label: 'Next', icon: 'fa fa-chevron-right', onclick: $scope.onNextClicked, rightIcon: true});
@@ -185,7 +208,19 @@ coreApp.controller('SelectClientCtrl', function($scope, GlobalSvc, DaoSvc, Setti
 			$scope.image = sessionStorage.getItem('currentLicenceImage');
 			//TODO: Remove Once Cordova plugin for scanning system is done
 			$scope.RegNumber = 'HTT 091 GP';
+		} else if ($routeParams.screennum == 6){
+			$scope.view = 'form';
+			if ($scope.inspectiontype === 'technicalreport' || $scope.inspectiontype === 'customervisits' ) fetchGPs();
+			$scope.Form =  JSON.parse(sessionStorage.getItem('currentForm'));
+			$scope.Form.RegNumber = 'HTT 091 GP';
+			$scope.Form.VinNumber = sessionStorage.getItem('currentVinNumber');
+			$scope.Form.LicenceExpiryDate = '25 July 2017';
+			$scope.signatureLabel1 = ($scope.inspectiontype === 'supplierevaluation') ? 'Workshop Manager' : 'Inspector';
+			$scope.id = $scope.Form.FormType;
+			JsonFormSvc.buildForm($scope, $scope.id, $scope.id, false, true, 'form', 'view', '', $scope.Form.JSON);
+			$scope.$emit('UNLOAD');
 		}
+
 	}
 	constructor();
 
