bf5d53a36bf31d8d777eadbda668ecdf3089dcde
Tevin <tevin@rapidtrade.biz>
2016-08-26 14:00:45 +0200

    version change to 1.79

--- js/app/Constants.js
+++ js/app/Constants.js
@@ -19,7 +19,7 @@ coreApp.constant('Settings', {
     imageUrl:           'http://app1.rapidtrade.biz/rest/Files/GetProductImage',
     */
 
-    version: '1.78',
+    version: '1.79',
     showVersion: true,
 
 
