ea6a91a430a09cfbd6dfeccc64ce5d6bb3a71e43
Tevin <tevin@rapidtrade.biz>
2016-08-26 21:26:43 +0200

    insert updynamic export logic for vehicle inspections

--- js/sginspect/controllers/ReportsCtrl.js
+++ js/sginspect/controllers/ReportsCtrl.js
@@ -8,6 +8,7 @@ coreApp.controller("ReportsCtrl", function ($scope, $routeParams, DaoSvc, $locat
 		{name: 'Supplier Evaluation', value: 'supplierevaluation'},
 		{name: 'After Service Inspection', value: 'afterserviceevaluation'}
 	];
+	 var newRow = {};
 	
 	function fetchJobs(){
 		var url = Settings.url + "Get?method=SGI_FormHeaders_readlist&UserID='" + GlobalSvc.getUser().UserID  + "'&FormType=''&startdate=''&enddate=''" + "&ExportedtoISO=''";
@@ -15,7 +16,7 @@ coreApp.controller("ReportsCtrl", function ($scope, $routeParams, DaoSvc, $locat
 		.success(function(json){
 			$scope.Jobs =json.map(function(e){
 				e.JSON = JSON.parse(e.JSON);
-				e.timeString = moment(e.FormDate).format('DD-MMM-YY')
+				e.timeString = moment(e.FormDate).format('DD-MMM-YY');
 				return e;
 			});
 			$scope.data = $scope.Jobs;
@@ -47,31 +48,77 @@ coreApp.controller("ReportsCtrl", function ($scope, $routeParams, DaoSvc, $locat
 
 	}
 	function getCSVData(scope){
-        var headings = getCSVHeadings(scope);
+        headings = getCSVHeadings(scope);
         if (!headings) return;
         var data = [];
+        var rows = {};
+        var new_headings = [];
         // Add data
         for (var i = 0 ; i < scope.Jobs.length; i++){
-            var newRow = {};
             for (var y = 0; y < headings.length; y++){
-                newRow[headings[y]] = (typeof scope.Jobs[i] ===  'object')  ? scope.Jobs[i].JSON[headings[y]] :  scope.Jobs[i][headings[y]];
+                if (headings[y].toLowerCase() === 'customer' || headings[y].toLowerCase() === 'branch'){
+                	newRow[headings[y]] = (!newRow[headings[y]]) ? scope.Jobs[i].JSON[headings[y]] : '';
+                }
+				rows = matchJsonToHeadings([headings[y]],scope.Jobs[i]).newRow;
+				new_headings = matchJsonToHeadings([headings[y]],scope.Jobs[i], y).headings;     
             }
-            data.push(newRow);
+            data.push(rows);
         }
-        scope.csvHeadings = headings;
+        scope.csvHeadings = new_headings;
         return data;
     }
 	 $scope.getCsvData = function(){
         return getCSVData($scope);
     };
 
+    function matchJsonToHeadings(prop, json){
+    	var newHeadings = [];
+    	if (prop === 'FormType'){ 
+    		var field = 'Report Type';
+    		switch (json[prop]){
+    			case 'audit' :
+    				newRow[field] = 'Audit Form';
+    				newHeadings.push(field);
+    				break;
+    			case 'supplierevaluation':
+    				newRow[field] = 'Supplier Evaluation'
+    				newHeadings.push(field);
+    				break;
+    			case 'technicalreport' :
+    				newHeadings.push(field);
+    				newRow[field] = 'Technical Report'
+    				break;
+    			case 'afterserviceevaluation' :
+    				newRow[field] = 'After Service Inspection';
+    				newHeadings.push(field);
+    				break;
+    			case 'customervisit' : 
+    				newRow[field] = 'Customer Visit';
+    				newHeadings.push(field);
+    				break;
+    		}
+    	}
+    		newRow['Date Submitted'] =  moment(json.FormDate).format('DD-MMM-YY');
+    		newRow['Technical Assessor'] = json.JSON.User_Name;
+    		newRow['ExportedtoISO'] = (!json['ExportedtoISO']) ? 'No' : 'Yes';
+    		newRow['Performed Location'] = json.JSON.Latitude + ' ' + json.JSON.Longitude;
+
+    		newHeadings.push('Date Submitted');
+    		newHeadings.push('Technical Assessor');
+    		newHeadings.push('ExportedtoISO');
+    		newHeadings.push('Performed Location');
+    	return {"headings" : newHeadings, "newRow": newRow}; 
+    }
+
     function getCSVHeadings(scope){
         var headingFields = [];
         for (var prop in $scope.Jobs[0]){
-            if ($scope.Jobs[0].hasOwnProperty(prop) && (prop.toLowerCase() === 'formtype' || prop.toLowerCase() === 'userid' || prop.toLowerCase() === 'formdate'|| prop.toLowerCase() === 'exportedtoiso')) headingFields.push(prop);
+        	// props used to fetch corresponding data  from object
+            if ($scope.Jobs[0].hasOwnProperty(prop) && (prop.toLowerCase() === 'formtype' || prop.toLowerCase() === 'formdate'|| prop.toLowerCase() === 'exportedtoiso')) headingFields.push(prop);
+            if (prop.toLowerCase() === 'userid') headingFields.push('User_Name');
         }
         for (var prop in $scope.Jobs[0].JSON){
-            if (prop.toLowerCase() === 'Customer' || prop.toLowerCase() === 'branch' || prop.toLowerCase() === 'latitude' || prop.toLowerCase() === 'longitude') headingFields.push(prop); 
+            if (prop.toLowerCase() === 'customer' || prop.toLowerCase() === 'branch' || prop.toLowerCase() === 'latitude' || prop.toLowerCase() === 'longitude') headingFields.push(prop); 
 
         }
         return headingFields;
