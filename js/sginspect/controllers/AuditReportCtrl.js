40a8153f969bae28d62855523a70cd3fe4365419
Tevin <tevin@rapidtrade.biz>
2016-08-30 16:10:37 +0200

    add changes to auditReportCtrl.js

--- js/sginspect/controllers/AuditReportCtrl.js
+++ js/sginspect/controllers/AuditReportCtrl.js
@@ -3,11 +3,29 @@ coreApp.controller("AuditReportCtrl", function($scope, $routeParams, DaoSvc, $lo
 
 	function fetchReport(){
 		$scope.Report = JSON.parse(sessionStorage.getItem('currentReportCache'));
+		switch ($scope.Report.FormType){
+			case 'audit' :
+				$scope.$emit('heading',{heading: 'Audit Form' , icon : 'fa fa-check'});
+				break;
+			case 'supplierevaluation':
+				$scope.$emit('heading',{heading: 'Supplier Evaluation' , icon : 'fa fa-check'});
+				break;
+			case 'technicalreport' :
+				$scope.$emit('heading',{heading: 'Technical Report' , icon : 'fa fa-check'});
+				break;
+			case 'afterserviceevaluation' :
+				$scope.$emit('heading',{heading: 'After Service Inspection' , icon : 'fa fa-check'});
+				break;
+			case 'customervisit' : 
+				$scope.$emit('heading',{heading: 'Customer Visit' , icon : 'fa fa-check'});
+	    		break;
+    	}
 	}
 
 	function constructor(){
 		if ($routeParams.mode){
 			$scope.$emit('left',{label: 'Back' , icon : 'glyphicon glyphicon-chevron-left', onclick: function(){window.history.back()}});
+			fetchReport();
 		}
 	}
 	constructor();
