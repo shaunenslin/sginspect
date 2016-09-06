184295f94f76de43db31fff936f4bf7d14e45a43
Shaun Enslin <shaun@rapidtrade.biz>
2016-09-06 16:22:23 +0200

    changed REST url

--- js/app/Constants.js
+++ js/app/Constants.js
@@ -25,8 +25,8 @@ coreApp.constant('Settings', {
 
 
 	//SG Admin
-    url: 'http://www.super-trade.co.za:8084/rest/index.php/',
-    dotnetPostUrl: 'http://www.super-trade.co.za:8084/rest/post/post.aspx',
+    url: 'http://www.super-trade.co.za:8084/sgirest/index.php/',
+    dotnetPostUrl: 'http://www.super-trade.co.za:8084/sgirest/post/post.aspx',
     supplierID: 'SGA',
     logo: 'http://www.sgadmin.co.za/logo/SG-Admin-sm.png',
     splash:  'img/groupbg.jpg',
@@ -57,12 +57,12 @@ coreApp.constant('Settings', {
         {table : 'Options',             getKey : function(item){return item.SupplierID + item.Name;},       index1:function(item){return item.Name;}},
         // {table : 'Tree',                getKey : function(item){return item.SupplierID + item.TreeID;},     index1:function(item){return item.Group;}, index2:function(item){return item.ParentTreeID;}, index3:function(item){return item.SortOrder;}},
         {table : 'Unsent',              getKey : function(item){return item.keyf;}, index1 : function(item){return 'undefined';} /* PLEASE DO NOT REMOVE index1 function: when loading logout screen we're checking unsent items by counting them on index1 column. For some reason on iOS local SQL, when we are inserting undefined as value it is not recorded that way  */ },
-        {table : 'SGIClient',          getKey : function(item){return item.ClientID;}, index1 : function(item){return item.ClientID;}, index2 : function(item){return item.Name;}}, 
+        {table : 'SGIClient',          getKey : function(item){return item.ClientID;}, index1 : function(item){return item.ClientID;}, index2 : function(item){return item.Name;}},
         {table : 'SGISuppliers',        getKey : function(item){return item.SupplierID + item.Name;}, index1 : function(item){return item.SupplierID;}, index2 : function(item){return item.Name;}},
         {table : 'InProgress',          getKey : function(item){return item.FormID},index1:function(item){return item.FormType;}, index2 : function(item){return item.JSON.Path;}, index3:function(item){return item.JobType;}},
     ],
 
-    workflow : { 
+    workflow : {
         audit : [
             {route:'selectclient'},
             {route:'scanlicense'},
