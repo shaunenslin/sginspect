a8b7225afffa9d0090354e1d1017145fbd949ec9
Tevin <tevin@rapidtrade.biz>
2016-09-19 10:29:23 +0200

    add logic to display signature

--- js/sginspect/controllers/CustomerVisitReportCtrl.js
+++ js/sginspect/controllers/CustomerVisitReportCtrl.js
@@ -8,7 +8,15 @@ coreApp.controller('CustomerVisitReportCtrl', function($scope, $routeParams,$loc
 		if ($scope.Report.JSON.SpecialRequests) $scope.SpecialRequests.map(function(e){ e.RequestDate = moment(e.RequestDate).format('YYYY/MM/DD')})
 		$scope.Report.JSON.timeString =  moment($scope.Report.FormDate).format('YYYY/MM/DD');
 		$scope.Report.FormDate = moment($scope.Report.FormDate).format('YYYY/MM/DD');
-		$scope.signature = $scope.imageUrl + $scope.Report.JSON.Signature;
+		var url  = $scope.imageUrl + $scope.Report.JSON.Signature;
+		$http.get (url)
+		.success(function(img){
+	    	data = "data:image/svg+xml;base64," + btoa(img);
+	    	$scope.signature = data;
+		})
+		.error(function(){
+			console.log('Error fetchig signature');
+		})
 		$scope.$emit('UNLOAD');
 	
 	}
